5413948c260dd0f4e034008dee4c9a00526f25f7
wj <562521514@qq.com>
2018-07-27 11:27:39 +0800

    'init'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>Try Element</p>
+    </el-dialog>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+<style>
+.hello .el-button{
+  font-size:30px; /*px*/
+}
+</style>
+
+
+
